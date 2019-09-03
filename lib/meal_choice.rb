e716394551523aff00e97ad469b4265b82e30120
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-03 21:48:12 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,9 @@
 def meal_choice.rb
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
-  
+end
+def meal_choice_with_vegetables_protein(protein, veg1, veg2)
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+def meal_choice_with_vegetables_protein_optional(protein ="meat", veg1, veg2)
+  puts "A plate"
